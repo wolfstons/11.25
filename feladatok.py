53c5d41b51008c59ac400e321319a731531f7c4f
wolfstons <paczi.balazs@gmail.com>
2025-11-25 14:29:37 +0100

    matrix

---
+++ feladatok.py
@@ -0,0 +1,11 @@
+def kiir(matrix):
+    i=0
+    while i < len(matrix=[]):
+        #print(matrix[i])
+        j=0
+        
+        while j < len(matrix[i]):
+            print(f"{matrix[i][j]:>3}",end=" ")
+            j+=1
+        i+=1
+        print("\n")
---
+++ main.py
@@ -0,0 +1,15 @@
+import feladatok
+
+matrix=[
+        [1,2,3,4]
+        ,[11,12,13,14]
+        ,[21,22,23,24]
+    ]   
+"""
+    print(matrix[0])
+    print(matrix[1][1])
+    print(matrix[len(matrix)-1][len(matrix[0])-1])
+"""
+"""írd ki a mátrix elemeit egymás alá rendezve!"""
+
+feladatok.kiir(matrix)
